9973073dd9a82fdc8a4721f0d83462750c4b4161
Kondor <konrad.kahl@wp.pl>
2017-01-29 12:23:07 +0100

    dropdown menu component events

--- src/app/components/popover/popover.component.ts
+++ src/app/components/popover/popover.component.ts
@@ -3,15 +3,28 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
     selector: 'popover',
     template: `
-    <ul>
+    <div (click)="triggerOpen($event)"><ng-content></ng-content></div>
+    <ul *ngIf="trigger">
       <li *ngFor="let item of items"><a [routerLink]="item.link">{{item.name}}</a></li>
     </ul>
-    `
+    `,
+    host: {
+        '(document: click)': 'onClick($event)'
+    }
 })
 
 export class PopoverComponent implements OnInit {
-
     @Input() items;
+    private trigger:boolean;
+
+    onClick(event) {
+        this.trigger = false;
+    }
+
+    triggerOpen(event) {
+        event.stopPropagation();
+        this.trigger = !this.trigger;
+    }
 
     ngOnInit() {
         console.log('Hello from Popover ', this.items);
